1f57501bf406ff10f565cca5875561303f88bdf9
Randy Pond <randito@gmail.com>
2013-07-11 14:44:31 -0500

    Adding getters and getter specs.

--- lib/mad_props/props.rb
+++ lib/mad_props/props.rb
@@ -1,3 +1,40 @@
 class MadProps::Props
 
+  def initialize(hash={})
+    raise ArgumentError.new 'not a hash' unless hash.is_a?(Hash)
+    hash.each do |key,value|
+      self.class.validate_key!(key)
+      self.class.validate_value!(value)
+      self.class.define_getter(key)
+      self.class.define_setter(key, value)
+      instance_variable_set("@#{key}", value)
+    end
+  end
+
+  private
+
+  def self.validate_key!(key)
+    # can't include reserve names
+    # can't use (), {}, = or .
+  end
+
+  def self.validate_value!(value)
+  end
+
+  def self.define_getter(key)
+    define_method(key) do
+      instance_variable_get("@#{key}")
+      # NOTE:  This is where the recursive lookup will occur
+    end
+  end
+
+  def self.define_setter(key, value)
+    define_method("#{key}=") do
+      validate_value!(value)
+      instance_variable_set("@#{key}", value)
+    end
+
+  end
+
+
 end
--- spec/mad_props/props_spec.rb
+++ spec/mad_props/props_spec.rb
@@ -1,19 +1,52 @@
 require 'mad_props'
-include MadProps
 
-describe Props do
-  context '#initialize' do
-    it 'from hash'
-  end
-  context 'get/set' do
-    it 'define getters'
-    it 'define setters'
-  end
-  describe 'with parent' do
-    it 'get inherited value'
-    it 'override inherited value'
-    describe 'with grandparent' do
-      it 'gets inherited value'
+describe MadProps::Props do
+
+  context 'from a hash' do
+
+    let(:hash) do
+      { name: 'randy', age: 5, cool: true, null: nil, pi: 3.14 }
     end
+    let(:updated_hash) do
+      # fyi, we are changing the types of some of the values.
+      { name: 'monkey', age: 1, cool: false, null: 'and void', pi: 'yummy' }
+    end
+
+    describe 'getters' do
+      subject { MadProps::Props.new(hash) }
+      it 'define getters' do
+        hash.keys.each do |key|
+          subject.respond_to?(key).should be_true
+        end
+      end
+      it 'implement getters' do
+        hash.each do |key,value|
+          subject.send(key).should == value
+        end
+      end
+    end
+
+    describe 'setters' do
+      it 'define setters'
+      it 'implement setters'
+    end
+
+    context 'with invalid property value types' do
+      it 'raise an exception'
+    end
+
+    context 'with invalid property names' do
+      it 'should raise an exception'
+    end
+
   end
+
+
+  # describe 'with parent' do
+  #   it 'get inherited value'
+  #   it 'override inherited value'
+  #   describe 'with grandparent' do
+  #     it 'gets inherited value'
+  #   end
+  # end
 end
