f25202b8257a4aa1a3ccf55b2b091ea2a88d21f7
Randy Pond <randito@gmail.com>
2013-07-12 12:49:25 -0500

    Extract getter and setter

--- lib/mad_props/props.rb
+++ lib/mad_props/props.rb
@@ -7,9 +7,7 @@ class MadProps::Props
 
   def initialize(hash={})
     raise ArgumentError.new 'not a hash' unless hash.is_a?(Hash)
-    hash.each do |key,value|
-      self.class.validate!(key, value)
-    end
+    hash.each { |key,value| self.class.validate!(key, value) }
     @hash = hash.with_indifferent_access
   end
 
@@ -30,15 +28,24 @@ class MadProps::Props
 
   private
 
+  def get_value(key)
+    @hash[key]
+  end
+
+  def set_value(key,value)
+    self.class.validate_value!(value)
+    @hash[key] = value
+  end
+
   def method_missing(method, *args)
     m = method.to_s
     key = m.sub(/=/,'')
     if @hash.has_key?(key)
       if m.index('=')
         raise ArgumentError.new("Only one argument") if args.size != 1
-        @hash[key] = args.first
+        set_value(key, args.first)
       else
-        @hash[key]
+        get_value(key)
       end
     else
       super(method, args)
